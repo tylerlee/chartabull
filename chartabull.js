2c310e12280300605119c630a4bb7521827d003c
tylerlee <tyler@orgsync.com>
2014-02-24 09:29:53 -0600

    added circle

--- chartabull.js
+++ chartabull.js
@@ -2,4 +2,19 @@
   'use strict';
   // Store a local reference to jQuery and Underscore.
   var $ = window.jQuery;
+  var _ = window._;
+
+  var chart = function(){
+    alert('hello');
+  }
+  var Chartabull = window.Chartabull = function(el, data){
+    _.extend(this, data);
+    this.buildContainer();
+  }
+
+  _.extend(Chartabull.prototype, {
+    buildContainer: function () {
+      return this;
+    }
+  });
 });
